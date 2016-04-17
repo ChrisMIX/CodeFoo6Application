3948247c2ecfc9a6707f731f4bec265a03a51de6
Chris Miklius <cmiklius@gmail.com>
2016-04-17 16:50:23 -0500

    finish date converter challenge

--- date_convert/lib/date_convert.rb
+++ date_convert/lib/date_convert.rb
@@ -1,7 +1,7 @@
-require "date_convert/version"
-
+require "../lib/date_convert/version.rb"
+require "..lib/date_convert/fix_date.rb"
 module DateConvert
-  def fix_that_date(date)
 
-  end
 end
+
+puts DateConvert::fix_that_date("3/20/2016")
